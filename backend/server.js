b2f4c4c2dfd742f8b7a03273afeff1cdf5e1bd06
leontoys <svliyon@gmail.com>
2025-08-30 08:45:10 +1000

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import authRoutes from './routes/auth.routes.js'
 dotenv.config()
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -7,7 +8,8 @@ const app = express()
 app.get("/", (req, res) => {
     res.send('Hello World')
 })
+app.use("/api/auth",authRoutes)
 
 app.listen(PORT, () => {
-    console.log('app running on PORT 5000')
+    console.log(`app running on PORT ${PORT}`)
 })
