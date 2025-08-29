9c44f8b7c5ce915e9ee79a8f7018839e687d3be6
leontoys <svliyon@gmail.com>
2025-08-30 08:37:10 +1000

    created frontend and backend base

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+    console.log('app running on PORT 5000')
+})
