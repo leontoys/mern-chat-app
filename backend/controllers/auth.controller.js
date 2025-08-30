4df23da4f3d3578076ab1ff32216a483d6aa33cd
leontoys <svliyon@gmail.com>
2025-08-31 07:37:47 +1000

    logout done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -83,5 +83,13 @@ export const login = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-    res.send("logout user")
+    try {
+      //clear cookie
+      res.cookie("jwt", "", { maxAge: 0 })
+      res.status(200).json({message:'Logged out successfully'})
+
+    } catch (error) {
+      console.log('Error in logout controller', error)
+      res.status(500).json({message:'Internal server error'})
+    }
 }
