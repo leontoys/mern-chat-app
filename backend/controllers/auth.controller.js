a90ef9c2d96fb244d8897864b25dd4750cdc01ca
leontoys <svliyon@gmail.com>
2025-08-31 06:51:40 +1000

    created user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,47 @@
-export const signup = (req, res) => {
-    res.send("sign up")
+import User from "../models/user.model.js"
+
+export const signup = async (req, res) => {
+    try {
+    const{fullName,userName,password,confirmPassword,profilePic,gender} = req.body;
+
+  if (password !== confirmPassword) {
+    return res.status(400).json({ error: "Passwords dont match" });
+  }
+
+  const user = await User.findOne({ userName });
+    if (user) {
+    console.log(user.fullName,user.userName,user.password,user.gender,user.profilePic)
+    return res.status(400).json({ error: "Username already exists" });
+  }
+
+  //hash password
+
+  const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+  const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+
+  const newUser = new User({
+    fullName,
+    userName,
+    password,
+    gender,
+    profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+  });
+
+  await newUser.save();
+
+  res.status(201).json({
+    _id: newUser._id,
+    fullName: newUser.fullName,
+    userName: newUser.userName,
+    profilePic: newUser.profilePic,
+  });
+    
+    
+} catch (error) {
+        console.log('Error in signup controller', error)
+        res.status(500).json({message:'Internal server error'})
+}
+
 }
 
 export const login = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,7 @@ dotenv.config()
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send('Hello World')
-})
+app.use(express.json())//parse request.body
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT, () => {
