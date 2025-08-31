31d8c707b26f9eb5024aa1be6cfb30ee81f3a609
leontoys <svliyon@gmail.com>
2025-09-01 06:52:14 +1000

    created protected route middleware

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken"
+import User from "../models/user.model"
+
+const protectRoute = (req, res, next) => {
+    
+    //first get the token from the cookies 
+    //for this we need cookie parser to be added as middleware in server.js
+    try {
+
+        const token = req.cookies.jwt 
+
+        if (!token) {
+            return res.status(401).json({message:'Not authorized'})
+        }
+
+        //decode token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+        if (!decoded) {
+            return res.status(401).json({ message: 'Not authorized' })
+        }
+
+        //find the user from the decoded token
+        const user = User.findById(decoded.userId).select("-password")//because that's how we generated the token
+        //also remove password
+
+        if (!user) {
+            return res.status(401).json({message:'User not found'})
+        }
+
+        req.user = user //attach user to the request object ie, logged in sender id
+
+        next()
+        
+    } catch (error) {
+        console.error('Error in protect Route', error)
+        res.status(500).json({ message : 'Internal server error '})
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
 import connectDB from './db/connect.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())//parse request.body
+app.use(cookieParser())//to pare cookies 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
