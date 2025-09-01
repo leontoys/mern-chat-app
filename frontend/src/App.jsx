f3f88a3be56be83e5d916df88c9c56c98eb5be14
leontoys <svliyon@gmail.com>
2025-09-02 07:33:18 +1000

    homepage in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
+import Home from './pages/home/Home'
 
 const App = () => {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       {/*<Login />*/}
-      <SignUp/>
+      {/*<SignUp />*/}
+      <Home/>
     </div>
   )
 }
