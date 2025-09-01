7decf6eb8bd050921e954a301f908d69ddb08ee0
leontoys <svliyon@gmail.com>
2025-09-02 07:19:01 +1000

    signup in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
 
 const App = () => {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login/>
+      {/*<Login />*/}
+      <SignUp/>
     </div>
   )
 }
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -2,7 +2,47 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter 
+      backdrop-blur-lg bg-opacity-0'>
+        <h1 className='text-3xl font-semibold text-center text-gray-300'>
+          Sign Up
+          <span className='text-blue-500'> Chat App</span>
+        </h1>
+        <form >
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Full name</span>
+            </label>
+            <input type='text' placeholder='Enter Full name' className='w-full input input-bordered h-10' />
+          </div>
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Username</span>
+            </label>
+            <input type='text' placeholder='Enter Username' className='w-full input input-bordered h-10'/>
+          </div>
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Password</span>
+            </label>
+            <input type='password' placeholder='Enter Password' className='w-full input input-bordered h-10' />
+          </div>
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Confirm Password</span>
+            </label>
+            <input type='password' placeholder='Confirm Password' className='w-full input input-bordered h-10' />
+          </div>
+          <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+            Already have an account?
+          </a>
+          <div>
+            <button className='btn btn-block btn-sm mt-2'>Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
