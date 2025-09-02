f59833f202e70831099e98e16da15de2cc7a2fbb
leontoys <svliyon@gmail.com>
2025-09-03 07:40:38 +1000

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,16 @@ import React from 'react'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
 import Home from './pages/home/Home'
+import {Routes, Route} from "react-router-dom";
 
 const App = () => {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/*<Login />*/}
-      {/*<SignUp />*/}
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path='/login' element={ <Login/>} />
+      </Routes>
     </div>
   )
 }
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import GenderCheckbox from './GenderCheckbox'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
@@ -38,9 +39,9 @@ const SignUp = () => {
 
           <GenderCheckbox/>
 
-          <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+          <Link to='/login' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
             Already have an account?
-          </a>
+          </Link>
           <div>
             <button className='btn btn-block btn-sm mt-2'>Sign Up</button>
           </div>
