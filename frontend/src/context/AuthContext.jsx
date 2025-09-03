811f65582db230c99c460255c5c4a12a83f49084
leontoys <svliyon@gmail.com>
2025-09-04 07:16:38 +1000

    added use auth context

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,11 +1,19 @@
 import { createContext, useState } from "react";
 
+//step1 - create context
 const AuthContext = createContext()
 
-export const AuthContextProvider = (props) => {
+//step5 - create use context hook?
+export const useAuthContext = () => {
+    return useAuthContext(AuthContext)
+}
 
+//step2 - creae context provider 
+export const AuthContextProvider = (props) => {
+//step4 - create state that needs to be added to the provider and update this in the value
     const[authUser,setAuthUser] = useState(JSON.parse(localStorage.getItem("chatuser")) || null)
 
+//step 3 - context provider
     return (
         <AuthContext.Provider value={{ authUser, setAuthUser }}>
             {props.children}
