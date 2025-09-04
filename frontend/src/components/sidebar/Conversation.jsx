30ccbd8120a58670d9119c6cbd514e9e5be8f99d
leontoys <svliyon@gmail.com>
2025-09-05 07:49:35 +1000

    loaded users in the sidebar

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
-const Conversation = () => {
+const Conversation = ({conversation,emoji,lastIdx}) => {
   return (
       <>
           <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
               <div className='avatar online'>
                   <div className='w-12 rounded-full'>
-                      <img src='https://avatar.iran.liara.run/public/job/doctor/female'/>
+                      <img src={ conversation.profilePic} />
                   </div>
               </div>
               <div className='flex flex-col flex-1'>
                   <div className='flex gap-3 justify-between'>
-                      <p className='font-bold text-gray-200'>Jane Doe</p>
- 						<span className='text-xl'>🎃</span>
+                      <p className='font-bold text-gray-200'>{conversation.fullName}</p>
+                      <span className='text-xl'>{emoji}</span>
                   </div>
               </div>
           </div>  
-            <div className='divider my-0 py-0 h-1'></div>
+          {!lastIdx && <div className='divider my-0 py-0 h-1' />}
     </>
   )
 }
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import Conversation from './Conversation'
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from '../../utils/emojis';
 
 const Conversations = () => {
+
+  const {loading,conversations} = useGetConversations()
+
   return (
-    <div>
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+    <div className='py-2 flex flex-col overflow-auto'>
+      {conversations.map((conversation, idx) => (
+        <Conversation key={conversation._id}
+          conversation={conversation}
+          lastIdx={idx===conversation.length-1} //so that last conversation doesn't have the bar below it
+          emoji={ getRandomEmoji()} />
+      ))}
+      { loading ? <span className='loading loading-spinner mx-auto'></span> : null}
     </div>
   )
 }
