dc0a1300c4690c3e715b484e75a9104ba5f96a3d
leontoys <svliyon@gmail.com>
2025-09-07 07:19:08 +1000

    showing messages from both sender and receiver

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
 
-const Message = ({message}) => {
-  return (
-          <div className="chat chat-end">
-              <div className="chat-image avatar">
-                  <div className="w-10 rounded-full">
-                      <img alt="Tailwind CSS chat bubble component"
-                          src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
-                      />
-                  </div>
-              </div>
-          <div className="chat-bubble text-white bg-blue-500">{ message.message}</div>
-          <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{message.createdAt }</div>
-          </div>
-  )
-}
+const Message = ({ message }) => {
+    const { authUser } = useAuthContext();
+    const { selectedConversation } = useConversation();
+    const fromMe = message.senderId === authUser._id;
+    const chatClassName = fromMe ? "chat-end" : "chat-start";
+    const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+    const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-export default Message
+    return (
+        <div className={`chat ${chatClassName}`}>
+            <div className='chat-image avatar'>
+                <div className='w-10 rounded-full'>
+                    <img alt='Tailwind CSS chat bubble component' src={profilePic} />
+                </div>
+            </div>
+            <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+            <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{ message.createdAt }</div>
+        </div>
+    );
+};
+export default Message;
