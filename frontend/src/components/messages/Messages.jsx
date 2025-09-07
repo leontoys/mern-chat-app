6a3c6aaf560f8ac63d213c0ae01cde0aacb698fd
leontoys <svliyon@gmail.com>
2025-09-08 06:59:26 +1000

    auto scrolling works

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message'
 import useGetMessages from '../../hooks/useGetMessages.js';
 import MessageSkeleton from '../skeletons/MessageSkeleton.jsx';
 
 const Messages = () => {
   const { messages, loading } = useGetMessages()
-  console.log("messages",messages)
+  console.log("messages", messages)
+  const lastMessageRef = useRef()
+  
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" })      
+    },100)
+  },[messages])
 
   return (
     <div className='px-4 flex-1 overflow-auto'>
       {!loading && messages.length > 0 && messages.map((message) =>
-        <Message key={message._id} message={message} />)}
+        <div key={message._id} ref={lastMessageRef }>
+          <Message  message={message} />
+        </div>)}
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
       {!loading && messages.length === 0 && (
         <p className='text-center'>Send a message to start the conversation</p>
