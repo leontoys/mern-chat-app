2e7e33928b37f3b025f77037b3cc980b7a3eac5f
leontoys <svliyon@gmail.com>
2025-09-11 07:29:26 +1000

    avatar status works

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -18,12 +18,13 @@ const Conversation = ({ conversation, emoji, lastIdx }) => {
 
     return (
         <>
+
             <div
                 className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
 				${isSelected ? "bg-sky-500" : ""}
 			`}
                 onClick={() => setSelectedConversation(conversation)}>
-                <div className="avatar online">
+                <div className={`avatar ${isOnline?"avatar-online":""}`}>
                     <div className='w-12 rounded-full'>
                         <img src={conversation.profilePic} alt='user avatar' />
                     </div>
