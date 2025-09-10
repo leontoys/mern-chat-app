f1ef7c8aa58d14edd063fa48a1347ffdf22e1f02
leontoys <svliyon@gmail.com>
2025-09-11 07:22:04 +1000

    online status not working even directly

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -36,7 +36,7 @@ export const SocketContextProvider = ({ children }) => {
             }
 
         }
-    },[])
+    },[authUser])
 
     return(
     <SocketContext.Provider value={{ socket, onlineUsers }}>
