1d11fbae318723b362f4ec8fb53e7d02e7135aab
leontoys <svliyon@gmail.com>
2025-09-12 06:45:34 +1000

    shake animation and sound added

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/socketContext";
 import useConversation from "../zustand/useConversation.js";
+import notificationSound from "../assets/sounds/notification.mp3"
 
 const useListenMessages = () => {
     const { socket } = useSocketContext()
@@ -8,10 +9,14 @@ const useListenMessages = () => {
     
     useEffect(() => {
         socket?.on("newMessage", (newMessage) => {
-            console.log("messages",messages)
+            console.log("messages", messages)
+            const sound = new Audio(notificationSound)
+            sound.play()
+            newMessage.shouldShake = true
             setMessages([...messages,newMessage])
         })
 
+        //clean up function - so that we listen it only once
         return ()=>socket?.off("newMessage")
     },[socket,setMessages,messages])
 }
