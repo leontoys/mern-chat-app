7d6b0626105c06c899c75319efde3f94dd8b1e16
leontoys <svliyon@gmail.com>
2025-09-12 07:27:55 +1000

    changed socket uri

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173"],//this we don't have to change even after deploying, change only in frontend
     methods: ["GET", "POST"],
   },
 });
--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -13,10 +13,11 @@ export const SocketContextProvider = ({ children }) => {
     const [socket, setSocket] = useState(null);
     const [onlineUsers, setOnlineUsers] = useState([]);
     const { authUser } = useAuthContext();
+    const uri = "https://mern-chat-app-nznn.onrender.com/" //"http://localhost:5000"
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io(`${uri}`, {
                 query: {
                     userId: authUser._id //pass the authorised user's id to the backend
                     //with query parameter userId
